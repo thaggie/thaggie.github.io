f0afe144091a0ae90926f1d66eeb6ffe0d3a9e19
Tom Haggie <thaggie@gmail.com>
2018-11-23 00:31:32 -0800

    Added the johari tool

---
+++ johari/precache-manifest.e8d25cf8b6280c05dcedb78446f60a4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b6dd671263dfe97e623d",
+    "url": "/static/js/main.b6dd6712.chunk.js"
+  },
+  {
+    "revision": "20094b02aa6e4b1c5dee",
+    "url": "/static/js/1.20094b02.chunk.js"
+  },
+  {
+    "revision": "b6dd671263dfe97e623d",
+    "url": "/static/css/main.17fd4730.chunk.css"
+  },
+  {
+    "revision": "18dcd83e388f0cabd67f3353a1430749",
+    "url": "/index.html"
+  }
+];
