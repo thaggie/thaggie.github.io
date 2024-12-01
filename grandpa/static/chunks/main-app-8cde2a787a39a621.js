6f0b537969fe0acd8216b18e50f134045dac1055
Tom Haggie <thaggie@gmail.com>
2024-12-01 02:13:06 -0800

    grandpa

---
+++ grandpa/static/chunks/main-app-8cde2a787a39a621.js
@@ -0,0 +1,22 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+  [744],
+  {
+    9973: function (e, n, t) {
+      Promise.resolve().then(t.t.bind(t, 4163, 23)),
+        Promise.resolve().then(t.t.bind(t, 3231, 23)),
+        Promise.resolve().then(t.t.bind(t, 8233, 23)),
+        Promise.resolve().then(t.t.bind(t, 5505, 23)),
+        Promise.resolve().then(t.t.bind(t, 368, 23)),
+        Promise.resolve().then(t.t.bind(t, 8694, 23));
+    },
+  },
+  function (e) {
+    var n = function (n) {
+      return e((e.s = n));
+    };
+    e.O(0, [343, 6], function () {
+      return n(2619), n(9973);
+    }),
+      (_N_E = e.O());
+  },
+]);
